9b8067dfac845ede77b3c4e88e995821049c014c
Daveto <David.palanco@gmail.com>
2018-05-08 18:22:31 +0200

    commit

---
+++ Proyecto Parking/src/parking/Principal.java
@@ -0,0 +1,44 @@
+package parking;
+
+import java.util.Scanner;
+
+public class Principal {
+	public static void main(String[] args) {
+		//objeto
+		Parking parkco;
+		Scanner in = new Scanner(System.in);
+		int opcion;
+
+		do {
+			do {
+				System.out.println(
+						"Elige una opción:" + "\n	1. Entrada." + "\n	2. Salida." + "\n	3. ." + "\n	4. ." + "\n	5. ." + "\n	6. .");
+				opcion = in.nextInt();
+			} while (opcion < 1 || opcion > 6);
+
+			switch (opcion) {
+			case 1:
+				//	objeto.metodo(in);
+				
+
+				break;
+			case 2:
+				//objeto.metodo(in);
+				break;
+			case 3:
+				//objeto.metodo(in);;
+				break;
+			case 4:
+				//Metodos
+				break;
+			case 5:
+				//Metodos
+				break;
+
+			}
+		} while (opcion != 6);
+
+		in.close();
+	}
+
+}
